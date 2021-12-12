4678550035ca66ca91a0aef2ed90d78103c165cb
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2021-12-12 17:21:42 -0300

    Relation movie & user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,41 +6,46 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '@prisma/client';
+import { AuthGuard } from '@nestjs/passport';
+import AuthUser from 'src/auth/auth-user.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-
-  @Post('')
+  @Post('register')
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
-
   @Get()
   findAll(): Promise<any[]> {
     return this.usersService.findAll();
   }
-
   @Get(':id')
   findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOne(id);
   }
-
-  @Patch(':id')
+  @UseGuards(AuthGuard())
+  @Patch('edit/:id')
   update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
     return this.usersService.update(id, updateUserDto);
   }
-
-  @Delete(':id')
+  @UseGuards(AuthGuard())
+  @Delete('delete/:id')
   remove(@Param('id') id: string): Promise<{ message: string }> {
     return this.usersService.remove(id);
+  }
+  @UseGuards(AuthGuard())
+  @Patch('add/:id')
+  addfilmes(@AuthUser() user: User, @Param('id') id: string) {
+    return this.usersService.addFilme(user, id);
   }
 }
